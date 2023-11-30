10a8c244fba70ecdbb398cc44925c5c4f412e75e
Jevon Kendon <KendonJ@moe.govt.nz>
2023-12-01 11:38:04 +1300

    initial

---
+++ server/vendor-server.js
@@ -0,0 +1,38 @@
+﻿const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const products = [
+  { type: "Coke", price: "5.5", currency: "NZD" },
+  { type: "Pepsi", price: "5.3", currency: "NZD" },
+  { type: "Fanta", price: "4.5", currency: "NZD" },
+];
+
+const requestListener = function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  switch (req.url) {
+    case "/vendor":
+      res.writeHead(200);
+      res.end(JSON.stringify(products));
+      break
+    case "/vendor/coke":
+      res.writeHead(200);
+      res.end(JSON.stringify(products[0]));
+      break
+    case "/vendor/pepsi":
+      res.writeHead(200);
+      res.end(JSON.stringify(products[1]));
+      break
+    case "/vendor/fanta":
+      res.writeHead(200);
+      res.end(JSON.stringify(products[2]));
+      break
+  }
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ test/test.js
@@ -0,0 +1,39 @@
+﻿const axios = require('axios')
+
+async function testCoke() {
+  console.log("Test:Coke - Start");
+
+  const response = await axios.get("http://localhost:8000/vendor/coke");
+  // console.log(response.data);
+
+  if (response.status !== 200) {
+    console.error("Error status code");
+  }
+
+  const expected = { type: 'Coke', price: '5.5', currency: 'NZDa' };
+  const actual = response.data;
+
+  if (JSON.stringify(expected) !== JSON.stringify(actual)) {
+    console.error("Test:Coke - Failed expected/actual", expected, actual);
+  }
+
+  console.log("Test:Coke - End");
+}
+
+async function runTests() {
+  console.log("Tests - Start");
+
+  await testCoke();
+
+  console.log("Tests - End");
+}
+
+async function main() {
+  console.log("Starting ...");
+
+  await runTests();
+}
+
+main()
+  .then(v => console.log("Finished"))
+  .catch(err => console.error(err));
